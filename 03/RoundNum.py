0e58a4b1d6047362eee7a59a4c43bede074dcb74
srea <894691042@qq.com>
2019-09-25 00:11:31 +0800

    03-start roundNum

---
+++ 03/RoundNum.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Srea
+# @Date:   2019-09-24 21:43:24
+# @Last Modified by:   srea
+# @Last Modified time: 2019-09-24 21:43:52
+
+
+#****************************#
+#对浮点数执行指定精度的舍入运算
+#对于简单的舍入运算，使用内置的 round(value, ndigits) 函数即可
+#
+#****************************#
