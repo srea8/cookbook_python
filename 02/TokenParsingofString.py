aae11cbca4214affe3a892b853268c0093f19a5e
szj <shenzhijie@corp.netease.com>
2019-09-24 18:01:41 +0800

    tokenString

---
+++ 02/TokenParsingofString.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Srea
+# @Date:   2019-09-24 17:17:43
+# @Last Modified by:   shenzhijie
+# @Last Modified time: 2019-09-24 17:18:00
+
+#****************************#
+#想从左至右将其解析为一个令牌流
+#****************************#
